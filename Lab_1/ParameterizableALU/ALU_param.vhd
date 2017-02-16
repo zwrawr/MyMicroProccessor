9fb8be5da6102512d069e768387ef836847dfeb6
Zak West <zraw500@its.york.ac.uk>
2017-02-16 11:53:11 +0000

    \# Added lab one folder and Xilinx ise project  Committer: **Zak West** <zraw500@its.york.ac.uk>
     On branch **zwrawr-start-lab-one**
     \#\# Changes to be committed:
    *	new file:   .gitignore
    *	new file:   Lab_1/ParameterizableALU/ALU_param.vhd
    *	new file:   Lab_1/ParameterizableALU/DigEng.vhd
    *	new file:   Lab_1/ParameterizableALU/ParameterizableALU.xise
    *	new file:   Lab_1/ParameterizableALU/fuse.xmsgs

---
+++ Lab_1/ParameterizableALU/ALU_param.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use work.DigEng.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ALU_param is
+    Port ( A : in  STD_LOGIC_VECTOR (7 downto 0);
+           B : in  STD_LOGIC_VECTOR (7 downto 0);
+           X : in  STD_LOGIC_VECTOR (7 downto 0);
+           ctrl : in  STD_LOGIC_VECTOR (7 downto 0);
+           clk : in  STD_LOGIC;
+           O : out  STD_LOGIC_VECTOR (7 downto 0);
+           flags : out  STD_LOGIC_VECTOR (7 downto 0));
+end ALU_param;
+
+architecture Behavioral of ALU_param is
+
+begin
+
+
+end Behavioral;
+
